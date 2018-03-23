a380c5afe4f408f8b4d26be14f1d04cd069ba8cf
otto.leppanen <otto.leppanen@siili.com>
2018-03-23 15:49:06 +0200

    Add pre-commit. If doesn't working chmod +x pre-commit

--- frontend/src/components/Board.tsx
+++ frontend/src/components/Board.tsx
@@ -9,8 +9,6 @@ const Board: React.SFC<BoardProps> = (props: BoardProps) => {
 	const labels = props.labels;
 
 	const columns = () => {
-
-		
 		const content = (tasks: string[]) => {
 			return tasks.map(task => <Segment>{task}</Segment>);
 		};
