7f2c1b38a8430cd0b1ba805350aadf867d8ff1ea
otto.leppanen <otto.leppanen@siili.com>
2018-03-23 15:48:36 +0200

    Add pre-commit.

--- frontend/src/components/Board.tsx
+++ frontend/src/components/Board.tsx
@@ -9,6 +9,8 @@ const Board: React.SFC<BoardProps> = (props: BoardProps) => {
 	const labels = props.labels;
 
 	const columns = () => {
+
+		
 		const content = (tasks: string[]) => {
 			return tasks.map(task => <Segment>{task}</Segment>);
 		};
